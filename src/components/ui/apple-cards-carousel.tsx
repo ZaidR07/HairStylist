2aa44c4ba3b7cda2c0c2c14b0b8b5f348f9ff914
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2024-12-06 12:54:38 +0530

    Fixed Deployment Errors2

--- src/components/ui/apple-cards-carousel.tsx
+++ src/components/ui/apple-cards-carousel.tsx
@@ -14,7 +14,7 @@ import {
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
 import Image, { ImageProps } from "next/image";
-import { useOutsideClick } from "@/hooks/use-outside-click.ts";
+import { useOutsideClick } from "@/hooks/use-outside-click";
 
 interface CarouselProps {
   items: JSX.Element[];
